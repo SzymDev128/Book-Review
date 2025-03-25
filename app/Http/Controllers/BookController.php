dc77ecff5968c98651289ca58d31f0d789702fed
Szymon Banaszek <szymon.banaszek128@onet.pl>
2025-03-25 23:30:21 +0100

    Reviews display

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -23,7 +23,7 @@ class BookController extends Controller
                 'highest_rated_last_month' => $books->highestRatedLastMonth(),
                 'highest_rated_last_6months' => $books->highestRatedLast6Months(),
 
-                default => $books->latest(),
+                default => $books->withCount('reviews')->withAvg('reviews', 'rating')->latest(),
             };
             $books = $books->get();
         return view('books.index', ['books' => $books]);  //compact('books')
@@ -48,9 +48,15 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view('books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn ($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
